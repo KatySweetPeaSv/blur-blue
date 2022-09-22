111f32e30d43ff94742980dfdb716b797740a431
Nelson Rosales <nelson.rosales83@gmail.com>
2022-09-22 17:52:15 +0200

    feat: delete item from checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { useSelector, useDispatch } from "react-redux";
+import { deleteFromCart } from "../redux/actions.js";
 
 const Checkout = () => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
-  console.log(cart);
+  const initialValue = 0;
+  // sum of all the prices in the cart
+  const total = cart.reduce((acc, item) => acc + item.price, initialValue);
+  console.log(total);
   return (
     <div className="w-[25rem] m-auto text-center p-10 items-center font-lato">
       <div className="border-GrayishBlack border-[1px] m-auto flex flex-col py-5 justify-center rounded gap-4  bg-[#fcf8f5]">
@@ -14,7 +18,7 @@ const Checkout = () => {
           cart.map((item) => {
             return (
               <>
-                <div className="flex justify-around flex-wrap border-y-[1px] border-GrayishBlack py-2">
+                <div className="flex justify-around flex-wrap  py-2">
                   <div>
                     <div className="flex justify-center">
                       <img
@@ -34,6 +38,7 @@ const Checkout = () => {
                     <TrashIcon
                       className="w-5 hover:text-Reddish cursor-pointer transition duration-300"
                       title="Eliminar"
+                      onClick={() => dispatch(deleteFromCart(item.id))}
                     />
                   </div>
                 </div>
@@ -42,11 +47,12 @@ const Checkout = () => {
           })}
         <div className="grid grid-cols-1 gap-1">
           <p>Total a pagar</p>
-          <p className="font-bold text-green-500">US$35.29</p>
+          <p className="font-bold text-green-500">{total}</p>
           <button className="mx-[55px]  bg-green-500 hover:bg-green-300 capitalize">
             Comprar Ya
           </button>
         </div>
+
         <div>
           <a href="#" className="text-xs capitalize">
             Ver todos
