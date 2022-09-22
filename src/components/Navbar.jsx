d701d4c2811571957aec1e53c7bf77135174406b
Erick Martir <erick.edenilson92@gmail.com>
2022-09-22 10:11:19 -0600

    accepted incoming changes from Nelsons branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,12 @@
-import React from "react";
-import CardGrid from "./card-grid/CardGrid";
-import { Link } from "react-router-dom";
-import { ShoppingCartIcon } from "@heroicons/react/24/solid";
-import { useSelector } from "react-redux";
+import React from 'react';
+import CardGrid from './card-grid/CardGrid';
+import { Link } from 'react-router-dom';
+import { ShoppingCartIcon } from '@heroicons/react/24/solid';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
   const cart = useSelector((state) => state.cart);
   return (
-<<<<<<< HEAD
-    <div className='w-screen z-10 bg-GrayishBlack fixed drop-shadow-lg'>
-        <div className='flex justify-between items-center w-full h-full px-2  text-white'>
-                            <div className='py-5'>
-                              <img className='w-[9rem] cursor-pointer' src={Logo} alt="company logo" />
-                            </div>
-                        <div className='flex items-center '>
-                                    <ul className='flex'>
-                                        <li className='font-oswald'>Figuras</li>
-                                        <li className='font-oswald'>Franquicias</li>
-                                        <li className='font-oswald'>Pre-Ordenes</li>
-                                        <li className='font-oswald'>Recién Llegadas</li>
-                                        <li className='font-oswald'>Ofertas</li>
-                                    </ul>
-                        </div>
-                        <div className='flex items-center'>
-                            <div className='mr-4 flex'> 
-                            < ShoppingCartIcon className=' w-7 mr-8 text-white cursor-pointer' title='Carrito'  />
-                            <a href="#" className='pCounter ml-[-44px] mb-[38px] text-center text-xs'>0</a>
-                            </div>
-                            
-                            <button className='px-4 py-2 mr-4 font-oswald'>Ingresar</button>
-                            <button className='px-4 py-2 mr-4 font-oswald'>Registrarme</button>
-                        </div>
-        </div>
-    </div>
-  )
-}
-=======
     <div className="w-screen z-10 bg-GrayishBlack fixed overflow-hidden top-0 drop-shadow-lg mb-5 p-2">
       <div className="flex justify-between items-center w-full h-full px-2  text-white">
         <div className="flex items-center ">
@@ -44,7 +15,6 @@ const Navbar = () => {
               Mizu No KoKyú
             </h1>
           </Link>
->>>>>>> origin/Nelson
 
           <ul className="flex">
             <li className="font-oswald">Figuras</li>
